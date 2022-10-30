e8c15c07bf02825da3dc76ad1b440927b019c601
Your Name <you@example.com>
2022-10-30 09:52:14 +0900

    第6課題に向けて

---
+++ src/main/java/com/example/FirstSBHelloWorld/FirstSbHelloWorldApplication.java
@@ -0,0 +1,13 @@
+package com.example.FirstSBHelloWorld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FirstSbHelloWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FirstSbHelloWorldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FirstSBHelloWorld/FirstSbHelloWorldApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FirstSBHelloWorld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirstSbHelloWorldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
