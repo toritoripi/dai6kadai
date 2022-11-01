3adb94aff191723ced5ddadff51ecf929d92947f
さいとう <114993632+toritoripi@users.noreply.github.com>
2022-11-01 19:26:00 +0900

    時刻表示の追加

--- src/main/java/com/example/FirstSBHelloWorld/HelloWorldController.java
+++ src/main/java/com/example/FirstSBHelloWorld/HelloWorldController.java
@@ -3,6 +3,11 @@ package com.example.FirstSBHelloWorld;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.time.format.DateTimeFormatter;
+import java.util.HashMap;
+import java.util.Map;
+import java.time.LocalDateTime;
+
 @RestController
 public class HelloWorldController {
 
@@ -10,6 +15,17 @@ public class HelloWorldController {
     public String hello() {
         String message = "hello world!";
         return  message;
+    }
+
+    @GetMapping("/time")
+    public Map<String, String> time() {
+        Map<String, String> replyMap = new HashMap<String, String>();
+        replyMap.put("Greeting", "hello world!");
+
+        String formatNowDate = DateTimeFormatter.ofPattern("yyyy年MM月dd日 HH時mm分ss秒 E曜日").format(LocalDateTime.now());
+
+        replyMap.put("time", formatNowDate);
+        return replyMap;
     }
 
 }
